245cbcc4892af2f138349078014f77b57630525b
Maciej Oleksy <maciejoleksy@gmail.com>
2019-12-04 16:41:27 +0100

    Add JS variables

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let computerMove = 'kamien';
+let playerMove = 'papier';
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to noyce, to wygrywasz!');
